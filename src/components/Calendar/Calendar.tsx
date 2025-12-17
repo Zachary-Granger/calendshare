6022e528ca63587caf75dad0c4c3465e326bd06c
Zachary Granger <zachary.g@hotmail.ca>
2025-12-16 20:52:34 -0800

    tile that expands correctly over selection

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import classes from './Calendar.module.css'
+import EventTile from "../EventTile/EventTile";
 
 export default function Calendar() {
   const [selectStart, setSelectStart] = useState(-1);
-	const [selectEnd, setSelectEnd] = useState(-1);
+  const [selectEnd, setSelectEnd] = useState(-1);
+	const [selectTopPosition, setSelectTopPosition] = useState(0);
+	const [selectBottomPosition, setSelectBottomPosition] = useState(0);
+	const [selectLeftPosition, setSelectLeftPosition] = useState(0);
+	const [selectRightPosition, setSelectRightPosition] = useState(0);
+
 
   const timeTable = [
     "7 AM",
@@ -22,50 +29,61 @@ export default function Calendar() {
     "9 PM",
   ];
 
-  const beginSelectRange = (tableRowId: number) => {
-    console.log('Table Row ID: ', tableRowId);
+  const beginSelectRange = (tableRowId: number, tableCell: HTMLTableCellElement) => {
     setSelectStart(tableRowId);
-		setSelectEnd(tableRowId);
+    setSelectEnd(tableRowId);
+
+		// calculate the top and bottom positions of the cell to pass to the Event Tile
+		setSelectTopPosition(tableCell.getBoundingClientRect().top);
+		setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
+		setSelectLeftPosition(tableCell.getBoundingClientRect().left);
+		setSelectRightPosition(tableCell.getBoundingClientRect().right);
   }
 
-	const continueSelectRange = (tableRowId: number) => {
-		if (selectStart > -1 && tableRowId >= selectStart) {
-			console.log("select range expanded to :", tableRowId);
-			setSelectEnd(tableRowId);
-		}
-	}
+  const continueSelectRange = (tableRowId: number, tableCell: HTMLTableCellElement) => {
+    if (selectStart > -1 && tableRowId >= selectStart) {
+      setSelectEnd(tableRowId);
+    }
+		
+		// calculate the bottom position to expand the selection
+		setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
+  }
 
-	const endSelectRange = () => {
-		//for now let's just clear the selection
-		setSelectStart(-1);
-		setSelectEnd(-1);
-	}
+  const endSelectRange = () => {
+    //for now let's just clear the selection
+    setSelectStart(-1);
+    setSelectEnd(-1);
+
+		// clear existing values for top and bottom positions
+		setSelectTopPosition(0);
+		setSelectBottomPosition(0);
+  }
 
   return (
-		//worth noting that the onMouseUp event only triggers when the mouse is in the table. 
-		// Probably need to move this up to the app level or find another way to make it global
-    <table onMouseUp={() => endSelectRange()}>
-      <thead>
-        <tr>
-          <th>time</th>
-          <th>buttons</th>
-        </tr>
-      </thead>
-      <tbody>
-        {timeTable.map((entry, index) => (
-          <tr key={index}>
-            <td>{entry}</td>
-
-            {
-							!(index >= selectStart && index <= selectEnd) &&
-              <td style={{ height:"20px" }} onMouseEnter={() => continueSelectRange(index)}><button onMouseDown={() => beginSelectRange(index)}>BUTTON HERE</button></td>
-							||
-							(index == selectStart &&
-              <td style={{ height:"20px"}} rowSpan={selectEnd - selectStart + 1}><button style={{ width:"100%", height:"100%" }} onMouseDown={() => beginSelectRange(index)}>SECRET BUTTON</button></td>)
-            }
+    //worth noting that the onMouseUp event only triggers when the mouse is in the table. 
+    // Probably need to move this up to the app level or find another way to make it global
+    <div id="calendar" className={classes.parent}>
+      <table onMouseUp={() => endSelectRange()}>
+        <thead>
+          <tr>
+            <th className={classes.unselectable}>time</th>
+            <th className={classes.unselectable}>buttons</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {timeTable.map((entry, index) => (
+            <tr key={index}>
+              <td className={classes.unselectable}>{entry}</td>
+              <td className={classes.tableCell} onMouseDown={(e) => beginSelectRange(index, e.currentTarget)} onMouseOver={(e) => continueSelectRange(index, e.currentTarget)} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+			{selectStart > -1 && (
+			 <EventTile topPosition={selectTopPosition} bottomPosition={selectBottomPosition} leftPosition={selectLeftPosition} rightPosition={selectRightPosition}/>
+			)}
+
+    </div>
   )
 }
--- src/components/EventTile/EventTile.tsx
+++ src/components/EventTile/EventTile.tsx
@@ -1,5 +1,20 @@
-export default function EventTile() {
+import classes from './EventTile.module.css'
+
+interface EventTileProperties {
+  topPosition: number;
+  bottomPosition: number;
+  leftPosition: number;
+  rightPosition: number;
+}
+
+export default function EventTile(props: EventTileProperties) {
   return (
-    <button>EVENT BUTTON!</button>
+    <div 
+			className={classes.eventTile} 
+			style={{ top: props.topPosition, height: props.bottomPosition - props.topPosition, 
+				left: props.leftPosition, width: props.rightPosition - props.leftPosition }}>
+      This is a box on top
+    </div>
+    // <button className={classes.eventTile}>EVENT BUTTON!</button>
   )
 }
