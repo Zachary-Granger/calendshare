b904eb0a04552f007632b5c59dca6da9b2285a50
Zachary Granger <zachary.g@hotmail.ca>
2026-01-02 13:28:15 -0800

    #5 | Added logic to create event from selected time range

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,44 +1,74 @@
 import { useState } from "react";
 import classes from './Calendar.module.css'
 import EventTile from "../EventTile/EventTile";
+import PlaceholderEventTile from "../PlaceholderEventTile/PlaceholderEventTile";
+import EventConfirmationDialogue from "../EventConfirmationDialogue/EventConfirmationDialogue";
+
+interface ConfirmationDetails {
+  startTimestamp?: Date;
+  endTimestamp?: Date;
+}
 
 export default function Calendar() {
   const [selectStart, setSelectStart] = useState(-1);
-	const [selectTopPosition, setSelectTopPosition] = useState(0);
-	const [selectBottomPosition, setSelectBottomPosition] = useState(0);
-	const [selectLeftPosition, setSelectLeftPosition] = useState(0);
-	const [selectRightPosition, setSelectRightPosition] = useState(0);
+  const [selectEnd, setSelectEnd] = useState(-1);
+  const [selectTopPosition, setSelectTopPosition] = useState(0);
+  const [selectBottomPosition, setSelectBottomPosition] = useState(0);
+  const [selectLeftPosition, setSelectLeftPosition] = useState(0);
+  const [selectRightPosition, setSelectRightPosition] = useState(0);
 
-	const timeTable = [];
-	const timestamp = new Date();
-	timestamp.setHours(6,0,0,0);
-	for (let i = 6.0; i <= 18.0; i += 0.5) {
-		timeTable.push(new Date(timestamp.getTime()));
-		timestamp.setMinutes(timestamp.getMinutes() + 30);
-	}
+  const [confirmingEvent, setConfirmingEvent] = useState(false);
+  const [confirmationDetails, setConfirmationDetails] = useState<ConfirmationDetails>({})
 
-  const beginSelectRange = (tableRowId: number, tableCell: HTMLTableCellElement) => {
-    setSelectStart(tableRowId);
-
-		// calculate the top and bottom positions of the cell to pass to the Event Tile
-		setSelectTopPosition(tableCell.getBoundingClientRect().top);
-		setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
-		setSelectLeftPosition(tableCell.getBoundingClientRect().left);
-		setSelectRightPosition(tableCell.getBoundingClientRect().right);
+  const timeTable: Date[] = [];
+  const timestamp = new Date();
+  timestamp.setHours(6, 0, 0, 0);
+  for (let i = 6.0; i <= 18.0; i += 0.5) {
+    timeTable.push(new Date(timestamp.getTime()));
+    timestamp.setMinutes(timestamp.getMinutes() + 30);
   }
 
-  const continueSelectRange = (tableCell: HTMLTableCellElement) => {
-		// calculate the bottom position to expand the selection
-		setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
+  const beginSelectRange = (tableRowId: number, tableCell: HTMLTableCellElement) => {
+    if (!confirmingEvent) {
+      setSelectStart(tableRowId);
+      setSelectEnd(tableRowId);
+
+      // calculate the top and bottom positions of the cell to pass to the Event Tile
+      setSelectTopPosition(tableCell.getBoundingClientRect().top);
+      setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
+      setSelectLeftPosition(tableCell.getBoundingClientRect().left);
+      setSelectRightPosition(tableCell.getBoundingClientRect().right);
+    }
+  }
+
+  const continueSelectRange = (tableRowId: number, tableCell: HTMLTableCellElement) => {
+    if (!confirmingEvent) {
+      setSelectEnd(tableRowId);
+      // calculate the bottom position to expand the selection
+      setSelectBottomPosition(tableCell.getBoundingClientRect().bottom);
+    }
   }
 
   const endSelectRange = () => {
-    //for now let's just clear the selection
-    setSelectStart(-1);
+    setConfirmingEvent(true);
+    setConfirmationDetails({
+      startTimestamp: timeTable[selectStart],
+      endTimestamp: timeTable[selectEnd]
+    })
+  }
 
-		// clear existing values for top and bottom positions
-		setSelectTopPosition(0);
-		setSelectBottomPosition(0);
+  const confirmDialogue = () => {
+		alert("event saved!");
+		closeDialogue();
+  }
+
+  const closeDialogue = () => {
+    setConfirmingEvent(false);
+    setConfirmationDetails({});
+    setSelectStart(-1);
+    setSelectEnd(-1);
+    setSelectTopPosition(0);
+    setSelectBottomPosition(0);
   }
 
   return (
@@ -56,15 +86,19 @@ export default function Calendar() {
           {timeTable.map((entry, index) => (
             <tr key={index}>
               <td className={classes.unselectable}>{entry.toLocaleTimeString([], { hour: "numeric", minute: "numeric" })}</td>
-              <td className={classes.tableCell} onMouseDown={(e) => beginSelectRange(index, e.currentTarget)} onMouseOver={(e) => continueSelectRange(e.currentTarget)} />
+              <td className={classes.tableCell} onMouseDown={(e) => beginSelectRange(index, e.currentTarget)} onMouseOver={(e) => continueSelectRange(index, e.currentTarget)} />
             </tr>
           ))}
         </tbody>
       </table>
 
-			{selectStart > -1 && (
-			 <EventTile topPosition={selectTopPosition} bottomPosition={selectBottomPosition} leftPosition={selectLeftPosition} rightPosition={selectRightPosition}/>
-			)}
+      {selectStart > -1 && (
+        <PlaceholderEventTile topPosition={selectTopPosition} bottomPosition={selectBottomPosition} leftPosition={selectLeftPosition} rightPosition={selectRightPosition} />
+      )}
+
+      {confirmingEvent && (
+        <EventConfirmationDialogue confirmationDetails={confirmationDetails} confirmationCallback={confirmDialogue} cancellationCallback={closeDialogue} />
+      )}
 
     </div>
   )
